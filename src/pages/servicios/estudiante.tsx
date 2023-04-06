58614fc147eac51842b9f1d6011d1919e5f96474
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-04-06 16:36:37 -0600

    :art: refactor: Update página servicios/estudiante

--- src/pages/servicios/estudiante.tsx
+++ src/pages/servicios/estudiante.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import CardProductos from '@/components/Cards/CardProductos';
+import Image from 'next/image';
+import ImgEquipo from '@/public/images/imgequipo.jpg';
 
 const productos = [
   {
     title: 'Pasante',
     description:
-      'Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Buscamos estudiantes apasionados que quieran ganar experiencia práctica en la industria tecnológica. Únete a nuestro equipo como pasante y trabaja en proyectos desafiantes mientras aprendes de expertos en la industria.',
   },
   {
     title: 'Practicante',
     description:
-      'Disrupt inspire and think tank, social entrepreneur but preliminary thinking think tank compelling.',
+      'Si eres un estudiante interesado en la tecnología y en busca de oportunidades de realizar tu práctica profesional, Grupo ASICA es el lugar perfecto para ti.',
   },
   {
     title: 'Entrenamiento',
     description:
-      'A slice of heaven. O for awesome, this chocka full cuzzie is as rip-off as a cracker.',
+      'En Grupo ASICA, ofrecemos programas de entrenamiento prácticos y significativos para aquellos que buscan mejorar sus habilidades técnicas y profesionales.',
   },
 ];
 
@@ -71,40 +73,13 @@ export default function Estudiante() {
             </p>
           </div>
           <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            {/* <span className="relative inline-block">
-              <svg
-                viewBox="0 0 52 24"
-                fill="currentColor"
-                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-              >
-                <defs>
-                  <pattern
-                    id="d9d7687a-355f-4502-8ec4-7945db034688"
-                    x="0"
-                    y="0"
-                    width=".135"
-                    height=".30"
-                  >
-                    <circle
-                      cx="1"
-                      cy="1"
-                      r=".7"
-                    />
-                  </pattern>
-                </defs>
-                <rect
-                  fill="url(#d9d7687a-355f-4502-8ec4-7945db034688)"
-                  width="52"
-                  height="24"
-                />
-              </svg>
-              <span className="relative">Soluciones</span>
-            </span>{' '} */}
             Estudiante
           </h2>
-          <p className="text-base text-gray-700 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <p className="text-base text-justify text-gray-700 md:text-lg">
+            Si eres un estudiante o pasante en busca de una experiencia valiosa,
+            Grupo ASICA es el lugar perfecto para ti. Nos enorgullece ofrecer
+            oportunidades de aprendizaje prácticas y significativas para jóvenes
+            talentosos y apasionados.
           </p>
         </div>
         <div className="relative grid gap-5 sm:grid-cols-2 lg:grid-cols-3 content-center">
@@ -115,6 +90,149 @@ export default function Estudiante() {
               description={description}
             />
           ))}
+        </div>
+      </div>
+
+      <div className="px-6 py-6 bg-purple-700 dark:bg-gray-800 md:py-12 md:px-12 lg:py-16 lg:px-16 lg:flex lg :items-start">
+        <div className="xl:w-0 xl:flex-1 text-justify">
+          <h2 className="text-2xl font-extrabold leading-8 tracking-tight text-white sm:text-3xl sm:leading-9">
+            Únete a nuestro equipo
+          </h2>
+          <p className="max-w-2xl mt-3 text-lg leading-6 text-indigo-200 mb-6">
+            Trabaja en proyectos desafiantes mientras aprendes de expertos en la
+            industria y comienza a impulsar tu carrera hoy mismo.
+          </p>
+
+          <Image
+            src={ImgEquipo}
+            className="w-full h-72 sm:h-96 xl:h-[32rem] object-cover object-center rounded-sm"
+            alt=""
+          />
+        </div>
+
+        <div className="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
+          <div className="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+            <p className="text-white mt-10 mb-6 lg:mt-0">
+              Campos marcados con <span className="text-red-600">*</span> son
+              requeridos
+            </p>
+            <form
+              action=""
+              className="grid grid-cols-10 gap-6"
+            >
+              {/* input - nombre contacto */}
+              <div className="col-span-full">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-white"
+                >
+                  Nombre
+                  <span className="text-red-600">*</span>
+                </label>
+
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+                />
+              </div>
+
+              {/* input - correo */}
+              <div className="col-span-full">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-white"
+                >
+                  Correo Electrónico
+                  <span className="text-red-600">*</span>
+                </label>
+
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+                />
+              </div>
+
+              {/* input - telefono */}
+              <div className="col-span-full">
+                <label
+                  htmlFor="telefono"
+                  className="block text-sm font-medium text-white"
+                >
+                  Telefono
+                  <span className="text-red-600">*</span>
+                </label>
+
+                <input
+                  type="tel"
+                  id="telefono"
+                  name="last_name"
+                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+                />
+              </div>
+
+              {/* input - nombre contacto */}
+              <div className="col-span-full">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-white"
+                >
+                  Universidad / Colegio
+                  <span className="text-red-600">*</span>
+                </label>
+
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+                />
+              </div>
+
+              {/* input - mensaje */}
+              <div className="col-span-10">
+                <label
+                  htmlFor="mensaje"
+                  className="block text-sm font-medium text-white"
+                >
+                  Mensaje
+                </label>
+
+                <textarea
+                  id="mensaje"
+                  name="mensaje"
+                  required
+                  cols={30}
+                  rows={10}
+                  className="form-textarea mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+                ></textarea>
+              </div>
+
+              {/* button */}
+              <div className="col-span-full sm:flex sm:items-center sm:gap-4">
+                <button className="inline-flex justify-center items-center gap-1 shrink-0 rounded-md border border-blue-600 bg-blue-600 px-9 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
+                  Enviar
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 32 32"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M27.71 4.29a1 1 0 0 0-1.05-.23l-22 8a1 1 0 0 0 0 1.87l9.6 3.84l3.84 9.6a1 1 0 0 0 .9.63a1 1 0 0 0 .92-.66l8-22a1 1 0 0 0-.21-1.05ZM19 24.2l-2.79-7L21 12.41L19.59 11l-4.83 4.83L7.8 13l17.53-6.33Z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
