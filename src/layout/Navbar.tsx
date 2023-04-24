bf34745314544a681167e9ab0ae24c87c07787ea
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-04-24 13:29:56 -0600

    :recycle: fix: Remove hook useEffect

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import LogoAsica from '@/public/images/logo-asica.png';
