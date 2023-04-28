97f077bc70dadbb3782ea274fec7ba50754fd0ec
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-04-27 22:29:02 -0600

    :wrench: ci: Update config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-const colors = require('tailwindcss/colors');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      ...colors,
-    },
     extend: {
       colors: {
         primary: '#F9F9F9',
