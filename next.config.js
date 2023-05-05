8b50112b4611e3dd356703adae3ebd843d598a67
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-05-05 17:58:31 -0600

    🔧 build: Update config next

--- next.config.js
+++ next.config.js
@@ -33,6 +33,61 @@ const nextConfig = {
         destination: '/servicios/educacion',
         permanent: true,
       },
+      {
+        source: '/arpotecsa',
+        destination: '/productos/arpotecsa',
+        permanent: true,
+      },
+      {
+        source: '/freebalance',
+        destination: '/productos/freebalance',
+        permanent: true,
+      },
+      {
+        source: '/seguridad',
+        destination: '/productos/seguridad',
+        permanent: true,
+      },
+      {
+        source: '/huntress',
+        destination: '/productos/huntress',
+        permanent: true,
+      },
+      {
+        source: '/consultoria',
+        destination: '/productos/consultoria',
+        permanent: true,
+      },
+      {
+        source: '/sas',
+        destination: '/productos/sas',
+        permanent: true,
+      },
+      {
+        source: '/spk',
+        destination: '/productos/spk',
+        permanent: true,
+      },
+      {
+        source: '/jumpcloud',
+        destination: '/productos/jumpcloud',
+        permanent: true,
+      },
+      {
+        source: '/imedical',
+        destination: '/productos/imedical',
+        permanent: true,
+      },
+      {
+        source: '/riesgos',
+        destination: '/productos/riesgos',
+        permanent: true,
+      },
+      {
+        source: '/niff9',
+        destination: '/productos/niff9',
+        permanent: true,
+      },
     ];
   },
 };
