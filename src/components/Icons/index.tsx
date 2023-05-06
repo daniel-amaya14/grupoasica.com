7c7971c1267d3750f338ec84ee24999b0d0991ca
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-05-05 18:29:56 -0600

    ♻️ refactor: Update componentes

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -157,9 +157,9 @@ export const IconPymes = (props: IconProps) => {
       <path
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
         d="m8 9l5 5v7H8v-4m0 4H3v-7l5-5m1 1V4a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v17h-8m0-14v.01M17 7v.01M17 11v.01M17 15v.01"
       />
     </svg>
@@ -253,9 +253,9 @@ export const IconStudent = (props: IconProps) => {
       <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
       <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
         d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
       ></path>
     </svg>
