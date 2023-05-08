4b8f64fe1e4786aed57cfb9dc08c8e251610b417
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-05-08 16:52:47 -0600

    🎨 style: Update sección aprende de Home

--- src/components/Home/Aprende.tsx
+++ src/components/Home/Aprende.tsx
@@ -6,7 +6,7 @@ import ImgAprende from '@/public/svg/aprende.svg';
 export default function Aprende() {
   return (
     <section className="">
-      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+      <div className="flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
         {/*  */}
         <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           <Image
