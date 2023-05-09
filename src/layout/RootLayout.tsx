95f4adb9823f4ce97566312af0a163a564b8ff96
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-05-08 19:46:39 -0600

    ✨ feat: Add favicon

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '@/layout/Header';
 import Footer from '@/layout/Footer';
+import Head from 'next/head';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,13 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="./favicon.ico"
+          type="image/x-icon"
+        />
+      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
