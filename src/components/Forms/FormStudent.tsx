2d5308e48930b33f28558c0ad7b15ea875f09da4
Daniel Amaya <daniel_amaya@grupoasica.com>
2023-05-11 18:29:30 -0600

    ✨ feat: Add Formulario estudiante

---
+++ src/components/Forms/FormStudent.tsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const initialValues = {
+  name: '',
+  email: '',
+  telefono: '',
+  universidad: '',
+  mensaje: '',
+};
+
+const validate = (values: any) => {
+  const errors: any = {};
+  if (!values.name) {
+    errors.name = 'El campo es requerido';
+  } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.name)) {
+    errors.name = ' El nombre solo puede contener letras y espacios';
+  }
+
+  if (!values.email) {
+    errors.email = 'El campo es requerido';
+  } else if (
+    !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)
+  ) {
+    errors.email = 'El email no es válido';
+  }
+
+  if (!values.telefono) errors.telefono = 'El campo es requerido';
+  if (!values.universidad) errors.universidad = 'El campo es requerido';
+
+  return errors;
+};
+
+export default function FormStudent() {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={(values, { resetForm }) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            alert('Formulario enviado con éxito');
+            resetForm();
+            resolve(true);
+          }, 3000);
+          clearTimeout(3000);
+        });
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="grid grid-cols-10 gap-6">
+          {/* input - nombre contacto */}
+          <div className="col-span-full">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-white"
+            >
+              Nombre completo
+              <span className="text-red-600">*</span>
+            </label>
+            <Field
+              type="text"
+              id="name"
+              name="name"
+              className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+            />
+            <ErrorMessage
+              name="name"
+              component="small"
+              className="text-red-600"
+            />
+          </div>
+
+          {/* input - correo */}
+          <div className="col-span-full">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-white"
+            >
+              Correo Electrónico
+              <span className="text-red-600">*</span>
+            </label>
+            <Field
+              type="email"
+              id="email"
+              name="email"
+              className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+            />
+            <ErrorMessage
+              name="email"
+              component="small"
+              className="text-red-600"
+            />
+          </div>
+
+          {/* input - telefono */}
+          <div className="col-span-full">
+            <label
+              htmlFor="telefono"
+              className="block text-sm font-medium text-white"
+            >
+              Teléfono/Celular
+              <span className="text-red-600">*</span>
+            </label>
+            <Field
+              type="tel"
+              id="telefono"
+              name="telefono"
+              className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+            />
+            <ErrorMessage
+              name="telefono"
+              component="small"
+              className="text-red-600"
+            />
+          </div>
+
+          {/* input - nombre contacto */}
+          <div className="col-span-full">
+            <label
+              htmlFor="universidad"
+              className="block text-sm font-medium text-white"
+            >
+              Universidad / Colegio
+              <span className="text-red-600">*</span>
+            </label>
+            <Field
+              type="text"
+              id="universidad"
+              name="universidad"
+              className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+            />
+            <ErrorMessage
+              name="universidad"
+              component="small"
+              className="text-red-600"
+            />
+          </div>
+
+          {/* input - mensaje */}
+          <div className="col-span-10">
+            <label
+              htmlFor="mensaje"
+              className="block text-sm font-medium text-white"
+            >
+              Mensaje
+            </label>
+            <Field
+              as="textarea"
+              id="mensaje"
+              name="mensaje"
+              cols={30}
+              rows={10}
+              className="form-textarea mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
+            />
+          </div>
+
+          {/* button */}
+          <div className="col-span-full sm:flex sm:items-center sm:gap-4">
+            <button
+              className="inline-flex justify-center items-center gap-1 shrink-0 rounded-md border border-blue-600 bg-blue-600 px-9 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Enviar
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  fill="currentColor"
+                  d="M27.71 4.29a1 1 0 0 0-1.05-.23l-22 8a1 1 0 0 0 0 1.87l9.6 3.84l3.84 9.6a1 1 0 0 0 .9.63a1 1 0 0 0 .92-.66l8-22a1 1 0 0 0-.21-1.05ZM19 24.2l-2.79-7L21 12.41L19.59 11l-4.83 4.83L7.8 13l17.53-6.33Z"
+                />
+              </svg>
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+}
--- src/pages/servicios/educacion.tsx
+++ src/pages/servicios/educacion.tsx
@@ -3,6 +3,7 @@ import CardSoluciones from '@/components/Cards/CardSoluciones';
 import Image from 'next/image';
 import ImgEquipo from '@/public/images/imgequipo.webp';
 import { solucionesEducacion } from '@/data/listServices';
+import FormStudent from '@/components/Forms/FormStudent';
 
 export default function Estudiante() {
   return (
@@ -92,126 +93,11 @@ export default function Estudiante() {
 
         <div className="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
           <div className="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-            <p className="text-white mt-10 mb-6 lg:mt-0">
+            <p className="text-white mt-10 mb-6 text-sm lg:mt-0">
               Campos marcados con <span className="text-red-600">*</span> son
               requeridos
             </p>
-            <form
-              action=""
-              className="grid grid-cols-10 gap-6"
-            >
-              {/* input - nombre contacto */}
-              <div className="col-span-full">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium text-white"
-                >
-                  Nombre
-                  <span className="text-red-600">*</span>
-                </label>
-
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  required
-                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
-                />
-              </div>
-
-              {/* input - correo */}
-              <div className="col-span-full">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-white"
-                >
-                  Correo Electrónico
-                  <span className="text-red-600">*</span>
-                </label>
-
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
-                />
-              </div>
-
-              {/* input - telefono */}
-              <div className="col-span-full">
-                <label
-                  htmlFor="telefono"
-                  className="block text-sm font-medium text-white"
-                >
-                  Teléfono/Celular
-                  <span className="text-red-600">*</span>
-                </label>
-
-                <input
-                  type="tel"
-                  id="telefono"
-                  name="last_name"
-                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
-                />
-              </div>
-
-              {/* input - nombre contacto */}
-              <div className="col-span-full">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium text-white"
-                >
-                  Universidad / Colegio
-                  <span className="text-red-600">*</span>
-                </label>
-
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  required
-                  className="form-input mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
-                />
-              </div>
-
-              {/* input - mensaje */}
-              <div className="col-span-10">
-                <label
-                  htmlFor="mensaje"
-                  className="block text-sm font-medium text-white"
-                >
-                  Mensaje
-                </label>
-
-                <textarea
-                  id="mensaje"
-                  name="mensaje"
-                  required
-                  cols={30}
-                  rows={10}
-                  className="form-textarea mt-1 w-full rounded-md border-gray-200 text-sm shadow-sm bg-gray-100 focus:bg-white"
-                ></textarea>
-              </div>
-
-              {/* button */}
-              <div className="col-span-full sm:flex sm:items-center sm:gap-4">
-                <button className="inline-flex justify-center items-center gap-1 shrink-0 rounded-md border border-blue-600 bg-blue-600 px-9 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
-                  Enviar
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 32 32"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M27.71 4.29a1 1 0 0 0-1.05-.23l-22 8a1 1 0 0 0 0 1.87l9.6 3.84l3.84 9.6a1 1 0 0 0 .9.63a1 1 0 0 0 .92-.66l8-22a1 1 0 0 0-.21-1.05ZM19 24.2l-2.79-7L21 12.41L19.59 11l-4.83 4.83L7.8 13l17.53-6.33Z"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </form>
+            <FormStudent />
           </div>
         </div>
       </div>
